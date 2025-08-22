57c3256f245cbc4fd5eaeeee69a14e2f7328a067
Yusuf Yaşar <yusufyasar1301@gmail.com>
2025-08-22 15:15:34 +0300

    updating the icon and images path

--- kelime_bulucu/kelime_bulucu.py
+++ kelime_bulucu/kelime_bulucu.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import askyesno, showinfo, showwarning
 import sozluk, kelimeler
-
+import os
 
 class App(tk.Tk):
 
@@ -14,7 +14,9 @@ class App(tk.Tk):
 
         # main properties of the app
         self.title('Kelime Bulucu')
-        self.iconbitmap('./icons/sozluk.ico')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        sozluk_path = os.path.join(BASE_DIR, "icons", "sozluk.ico")
+        self.iconbitmap(sozluk_path)
         self.iconbitmap()
         self.size(350,300)
         self.main_screen()
@@ -40,7 +42,9 @@ class App(tk.Tk):
         self.special_padd2 = {'padx': 5, 'pady': 5}
 
         # image used in the start button
-        self.str_image = tk.PhotoImage(file='./images/start.png')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        start_path = os.path.join(BASE_DIR, "images", "start.png")
+        self.str_image = tk.PhotoImage(file=start_path)
 
         # features of main page contents 
         self.main_frame = tk.Frame(self,bg="Lavender")
@@ -120,7 +124,9 @@ class App(tk.Tk):
         self.guess_text.pack(fill=tk.X,expand=True,side=tk.LEFT,padx=10)
 
         # features of the guess button
-        self.guess_image = tk.PhotoImage(file='./images/guess.png')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        guess_path = os.path.join(BASE_DIR, "images", "guess.png")
+        self.guess_image = tk.PhotoImage(file=guess_path)
         self.guess_btn = tk.Button(self.topleftcenter_frame,image=self.guess_image,bg="SteelBlue",activebackground="SteelBlue",bd=0,cursor="hand2",command=self.guess)
         self.guess_btn.pack(expand=True,side=tk.LEFT)
 
